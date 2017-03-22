09b2502ae49697974fa3eaea012417f350f47f1b
krm22 <krmouat22@gmail.com>
2017-03-22 10:07:43 +0100

    initial commit

---
+++ public/app/app.js
@@ -0,0 +1,37 @@
+(function() {
+
+  var app =  angular.module('app', []);
+
+  app.provider('books', function() {
+         this.$get = function () {
+
+              var appName = 'Book Logger';
+              var appDesc = 'Track which books you need.';
+              var version =  '1.0';
+
+              if (includeVersionInTitle) {
+              appName += ' ' + version;
+              }
+
+
+              return {
+                appName: appName,
+                appDesc: appDesc
+              };
+        };
+
+        var includeVersionInTitle = false;
+        this.setIncludeVersionInTitle = function (value) {
+        includeVersionInTitle = value;
+    };
+
+
+     });
+
+     app.config(function(booksProvider){
+
+       booksProvider.setIncludeVersionInTitle(true);
+
+ });
+
+}());
