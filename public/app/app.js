108146bf4b54f62efbff07aedd02757647a16c5a
krm22 <krmouat22@gmail.com>
2017-03-22 12:03:04 +0100

    provider, factory, service, value & constants function added

--- public/app/app.js
+++ public/app/app.js
@@ -2,17 +2,24 @@
 
   var app =  angular.module('app', []);
 
-  app.provider('books', function() {
-         this.$get = function () {
+  app.provider('books', ['constants', function(constants) {
 
-              var appName = 'Book Logger';
-              var appDesc = 'Track which books you need.';
-              var version =  '1.0';
+        var includeVersionInTitle = false;
+        this.setIncludeVersionInTitle = function (value) {
+        includeVersionInTitle = value;
+        };
+
+        this.$get = function () {
+
+              var appName = constants.APP_TITLE;
+
+              var version = constants.APP_VERSION;
 
               if (includeVersionInTitle) {
               appName += ' ' + version;
               }
 
+              var appDesc = constants.APP_DESCRIPTION;
 
               return {
                 appName: appName,
@@ -20,18 +27,19 @@
               };
         };
 
-        var includeVersionInTitle = false;
-        this.setIncludeVersionInTitle = function (value) {
-        includeVersionInTitle = value;
-    };
 
 
-     });
 
-     app.config(function(booksProvider){
+     }]);
+
+     app.config(['booksProvider', 'constants', 'dataServiceProvider', function (booksProvider, constants, dataServiceProvider) {
 
        booksProvider.setIncludeVersionInTitle(true);
 
- });
+       console.log('title from constants service: ' + constants.APP_TITLE);
+
+       console.log(dataServiceProvider.$get);
+
+   }]);
 
 }());
