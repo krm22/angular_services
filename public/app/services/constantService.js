dbca62a5aafee99ded7d7fd6ada4bd1bec4cb2f7
krm22 <krmouat22@gmail.com>
2017-03-22 17:12:29 +0100

    promises, routing, cookies, log

---
+++ public/app/services/constantService.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+          .constant('constants', {
+               APP_TITLE: 'Book Logger',
+               APP_DESCRIPTION: 'Track which books you read.',
+               APP_VERSION: '1.0'
+        })
+}());
---
+++ public/app/services/loggerService.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+      .service('logger', BookAppLogger);
+
+  function LoggerBase() { }
+
+    LoggerBase.prototype.output = function(message) {
+        console.log('LoggerBase: ' + message);
+    };
+
+    function BookAppLogger() {
+
+            LoggerBase.call(this);
+
+            this.logBook = function(book) {
+                console.log('Book: ' + book.title);
+            }
+        }
+
+    BookAppLogger.prototype = Object.create(LoggerBase.prototype);
+
+
+
+
+}());
